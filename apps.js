ad6a630597052318ea73afde1848a5f14414e293
dushyant2901 <74450982+dushyant2901@users.noreply.github.com>
2021-01-11 19:31:07 +0530

    feat added button reference

---
+++ apps.js
@@ -0,0 +1,10 @@
+
+var input=document.querySelector("#input");
+var addBtn=document.querySelector("#add");
+var subbtn=document.querySelector("#sub");
+var output=document.querySelector("#output");
+
+
+
+addBtn.addEventListener("click",addClickHandler);
+subbtn.addEventListener("click",subClickHandler);
